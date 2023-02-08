66094857ac8025bf524cef79632bfc216776d356
TrOllOchamO <barnabe.perrault@gmail.com>
2023-02-08 16:17:09 +0100

    changes before asking for help on macros

--- src/game/world/chunk_renderer/blocks_resources.rs
+++ src/game/world/chunk_renderer/blocks_resources.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-pub mod Blocks {
+pub mod blocks {
     pub const AIR_BLOCK: u16 = 0;
     pub const DEBUG_BLOCK: u16 = 1;
     pub const DIRT_BLOCK: u16 = 2;
@@ -17,6 +17,22 @@ pub struct BlockInfos {
     pub color: Option<Color>,
 }
 
+// WIP -------------------------------------
+#[derive(Resource)]
+struct BlocksResource(Vec<BlockInfos>);
+
+#[macro_export]
+macro_rules! create_blocks_resource {
+    ($($block_infos:expr), +) => {
+        let mut temp_vec = Vec::new();
+        $(
+            temp_vec.push(&block_infos);
+        )+
+        temp_vec
+    };
+}
+// WIP -------------------------------------
+
 pub fn block_is_transparent(block_id: u16) -> bool {
     get_block_infos(block_id).is_transparent
 }
@@ -28,48 +44,61 @@ pub fn block_color(block_id: u16) -> Option<Color> {
 #[inline(always)]
 pub fn get_block_infos(block_id: u16) -> BlockInfos {
     match block_id {
-        Blocks::AIR_BLOCK => BlockInfos {
-            id: block_id,
+        blocks::AIR_BLOCK => BlockInfos {
+            id: blocks::AIR_BLOCK,
             name: "air_block",
             is_transparent: true,
             color: None,
         },
-        Blocks::DEBUG_BLOCK => BlockInfos {
-            id: block_id,
+        blocks::DEBUG_BLOCK => BlockInfos {
+            id: blocks::DEBUG_BLOCK,
             name: "debug_block",
             is_transparent: false,
             color: Some(Color::hsla(305., 1., 0.5, 1.)),
         },
-        Blocks::DIRT_BLOCK => BlockInfos {
-            id: block_id,
+        blocks::DIRT_BLOCK => BlockInfos {
+            id: blocks::DIRT_BLOCK,
             name: "dirt_block",
             is_transparent: false,
             color: Some(Color::hsla(42., 0.82, 0.3, 1.)),
         },
-        Blocks::GRASS_BLOCK => BlockInfos {
-            id: block_id,
+        blocks::GRASS_BLOCK => BlockInfos {
+            id: blocks::GRASS_BLOCK,
             name: "grass_block",
             is_transparent: false,
             color: Some(Color::hsla(127., 0.8, 0.39, 1.)),
         },
-        Blocks::STONE_BLOCK => BlockInfos {
-            id: block_id,
+        blocks::STONE_BLOCK => BlockInfos {
+            id: blocks::STONE_BLOCK,
             name: "stone_block",
             is_transparent: false,
             color: Some(Color::hsla(195., 0.02, 0.46, 1.)),
         },
-        Blocks::SAND_BLOCK => BlockInfos {
-            id: block_id,
+        blocks::SAND_BLOCK => BlockInfos {
+            id: blocks::SAND_BLOCK,
             name: "sand_block",
             is_transparent: false,
             color: Some(Color::hsla(64., 0.63, 0.62, 1.)),
         },
-        Blocks::WATER_BLOCK => BlockInfos {
-            id: block_id,
+        blocks::WATER_BLOCK => BlockInfos {
+            id: blocks::WATER_BLOCK,
             name: "water_block",
             is_transparent: true,
             color: Some(Color::hsla(204., 0.86, 0.48, 0.3)),
         },
         _ => panic!("The block id : {} does_not exist", block_id),
+    }
+}
+
+pub fn get_block_id(block_name: &str) -> u16 {
+    match block_name {
+        "air_block" => blocks::AIR_BLOCK,
+        "debug_block" => blocks::DEBUG_BLOCK,
+        "dirt_block" => blocks::DIRT_BLOCK,
+        "grass_block" => blocks::GRASS_BLOCK,
+        "stone_block" => blocks::STONE_BLOCK,
+        "sand_block" => blocks::SAND_BLOCK,
+        "water_block" => blocks::WATER_BLOCK,
+        _ => panic!("The block name : {} does_not exist", block_name),
     }
 }
